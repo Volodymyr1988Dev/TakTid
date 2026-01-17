d8132c9622be14d03903a340cae50f363ede73ca
Volodymyr <PropertyChernivtsy@gmail.com>
2026-01-17 19:18:14 +0100

    project-assignments delete AdminGuard

--- server/src/controllers/project.assignment.controller.ts
+++ server/src/controllers/project.assignment.controller.ts
@@ -15,17 +15,17 @@ import { ProjectAssignmentService } from '../services/ProjectAssignment';
 import { CreateProjectAssignmentDto } from '../types/project/project.assignment.create.dto';
 import { UpdateProjectAssignmentDto } from '../types/project/project.assignment.update.dto';
 import { QueryProjectAssignmentDto } from '../types/project/project.assignments.query.dto';
-import { UseGuards } from '@nestjs/common';
+//import { UseGuards } from '@nestjs/common';
 import type { AuthRequest } from '../types/index';
 //import { AuthGuard } from '../types/auth/guard';
-import { AdminGuard } from '../types/auth/admin.guard';
+//import { AdminGuard } from '../types/auth/admin.guard';
 
 @ApiTags('Project Assignments')
 //@UseGuards(AuthGuard)
 @Controller('project-assignments')
 export class ProjectAssignmentController {
   constructor(private readonly assignmentService: ProjectAssignmentService) {}
-  @UseGuards(AdminGuard)
+  //@UseGuards(AdminGuard)
   @Post()
   create(@Body() dto: CreateProjectAssignmentDto, @Req() req: AuthRequest) {
     if (!req.user) {
