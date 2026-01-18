0b01d4e422b625dcaac7bcda7e85802903a504cc
Volodymyr <PropertyChernivtsy@gmail.com>
2026-01-18 20:28:12 +0100

    cookie hope fix

--- server/src/types/auth/guard.ts
+++ server/src/types/auth/guard.ts
@@ -29,18 +29,16 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest<AuthRequest>();
     //const response = context.switchToHttp().getResponse<Response>();
 
-    //const coocki = request.cookies as {
-    //  access_token?: string;
-    const token = request.cookies?.access_token;
-    //refresh_token?: string;
-    //};
+    const coockie = request.cookies as {
+      access_token?: string;
+      //refresh_token?: string;
+    };
     //const token =
     //  cookies?.access_token ||
     //  request.headers.authorization?.replace('Bearer ', '');
     //const { access_token, refresh_token } = request.cookies ?? {};
     //const cookies = request.cookies as | { access_token?: string; refresh_token?: string }  | undefined;
-    //const token = coocki?.access_token;
-
+    const token = coockie?.access_token;
     //const token = cookies?.access_token;
     //const refreshToken = cookies?.refresh_token;
 
