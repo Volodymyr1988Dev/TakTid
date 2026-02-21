dd3a930de019cf32c82ddc39f331f79e1cff7326
Volodymyr <PropertyChernivtsy@gmail.com>
2026-02-21 16:04:20 +0100

    delete sw

--- client/src/main.ts
+++ client/src/main.ts
@@ -17,13 +17,7 @@ dayjs.extend(isBetween)
 
 const app = createApp(App);
 const pinia = createPinia();
-/*
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-  })
-}
-*/
+
 app.use(pinia);
 app.use(router);
 app.mount('#app')
