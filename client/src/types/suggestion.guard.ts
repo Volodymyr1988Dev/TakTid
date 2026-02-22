9cf4312c1368795d116f98c3b474e47f74e5017b
Volodymyr <PropertyChernivtsy@gmail.com>
2026-02-22 11:10:24 +0100

    hope fix Red_Day

--- client/src/types/suggestion.guard.ts
+++ client/src/types/suggestion.guard.ts
@@ -21,6 +21,7 @@ export function isAbsenceSuggestion(
     s.type === TimeKind.SICK ||
     s.type === TimeKind.VAB ||
     s.type === TimeKind.VACATION ||
-    s.type === TimeKind.DAY_OFF
+    s.type === TimeKind.DAY_OFF ||
+    s.type === TimeKind.RED_DAY
   )
 }
