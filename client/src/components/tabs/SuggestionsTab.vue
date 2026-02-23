721b56f12a3491fefffcf7d4f8728ee8779c6718
Volodymyr <PropertyChernivtsy@gmail.com>
2026-02-23 22:00:45 +0100

    style

--- client/src/components/tabs/SuggestionsTab.vue
+++ client/src/components/tabs/SuggestionsTab.vue
@@ -81,14 +81,7 @@ function selectSuggestion(s: TimeSuggestion) {
   flex-direction: column;
   gap: 12px;
   padding: 12px;
-}/*
-.card {
-  cursor: pointer;
 }
-.strong-type {
-  font-size: small;
-}
-*/
 .card {
   padding: 3px;
   border-radius: 12px;
@@ -101,49 +94,31 @@ function selectSuggestion(s: TimeSuggestion) {
   transform: translateY(-2px);
 }
 
-/* WORK */
 .card-work {
- /* background: #e0f2fe;*/
-  border: 1px solid #38bdf8;
   color: #0369a1;
 }
 
-/* MEETING */
 .card-meeting {
- /* background: #ede9fe;*/
-  border: 1px solid #8b5cf6;
   color: #5b21b6;
 }
 
-/* ABSENCE TYPES */
-
 .card-sick {
- /* background: #fee2e2;*/
-  border: 1px solid #ef4444;
   color: #991b1b;
 }
 
 .card-vacation {
- /* background: #dcfce7;*/
-  border: 1px solid #22c55e;
   color: #166534;
 }
 
 .card-vab {
- /* background: #fef9c3;*/
-  border: 1px solid #eab308;
   color: #854d0e;
 }
 
 .card-dayoff {
- /* background: #e0e7ff;*/
-  border: 1px solid #6366f1;
   color: #3730a3;
 }
 
 .card-red {
- /* background: #fecaca;*/
-  border: 1px solid #dc2626;
   color: #7f1d1d;
 }
 </style>
