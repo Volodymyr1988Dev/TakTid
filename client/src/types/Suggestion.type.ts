93390c44915c12c4b5c3a284c80304273dad367f
Volodymyr <PropertyChernivtsy@gmail.com>
2026-02-23 18:38:12 +0100

    fix suggestions

--- client/src/types/Suggestion.type.ts
+++ client/src/types/Suggestion.type.ts
@@ -1,19 +1,27 @@
-import type { WorkKind, AbsenceKind, MeetingKind } from './timeKind.enum'
+//import type { WorkKind, AbsenceKind, MeetingKind, TimeKind } from './timeKind.enum'
+import type { TimeKind } from './timeKind.enum'
 
 export type WorkSuggestion = {
-  type: WorkKind
+  type: typeof TimeKind.WORK | typeof TimeKind.EXTRA//WorkKind
   title: string
   projectId: string
   breakMinutes?: number
 }
 
 export type AbsenceSuggestion = {
-  type: AbsenceKind
+  //type: AbsenceKind
+   type:
+    | typeof TimeKind.SICK
+    | typeof TimeKind.VAB
+    | typeof TimeKind.VACATION
+    | typeof TimeKind.DAY_OFF
+    | typeof TimeKind.RED_DAY
   title: string
 }
 
 export type InternalSuggestion = {
-  type: MeetingKind
+  //type: MeetingKind
+  type: typeof TimeKind.MEETING
   title: string
 }
 
