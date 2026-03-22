c1caa600dbfe98c22c2149dc6e00c2e4efb4fd6e
Volodymyr <PropertyChernivtsy@gmail.com>
2026-03-22 20:27:12 +0100

    fix photo

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue'
-
+import { useAuthStore } from './stores/auth.store'; 
 import dayjs from 'dayjs'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import isoWeek from 'dayjs/plugin/isoWeek'
@@ -23,4 +23,15 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+const auth = useAuthStore()
+
+document.addEventListener("visibilitychange", async () => {
+  if (document.visibilityState === "visible") {
+    try {
+      await auth.initAuth()
+    } catch {
+      console.log("Session restore failed")
+    }
+  }
+})
 app.mount('#app')
