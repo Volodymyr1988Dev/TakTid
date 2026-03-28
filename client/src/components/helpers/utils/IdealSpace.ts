137a84834250df75fbadcde70f5e79e85bac5a05
Volodymyr <PropertyChernivtsy@gmail.com>
2026-03-28 20:03:06 +0100

    delete round05

--- client/src/components/helpers/utils/IdealSpace.ts
+++ client/src/components/helpers/utils/IdealSpace.ts
@@ -1,4 +1,4 @@
-import { round05 } from './hookMath'
+//import { round05 } from './hookMath'
 
 export function calculateIdealSpacing(L: number, ideal: number) {
   const exact = L / ideal
@@ -13,26 +13,32 @@ export function calculateIdealSpacing(L: number, ideal: number) {
 
   const lowerSpacing = L / lowerSegments
   const upperSpacing = L / upperSegments
-  const missing = round05(perfectLength - L)
-
+  //const missing = round05(perfectLength - L)
+  const missing = perfectLength - L
   return {
     ideal,
     exact,
     missing,
     lower: {
-      spacing: round05(lowerSpacing),
+      //spacing: round05(lowerSpacing),
+      spacing: lowerSpacing,
       segments: lowerSegments,
       //diff: round05(L - lowerSegments * ideal)
-      diff: round05(lowerDiff),
-      missing: round05(lowerDiff)
+      diff: lowerDiff,
+      //diff: round05(lowerDiff),
+      //missing: round05(lowerDiff)
+      missing: lowerDiff
     },
 
     upper: {
-      spacing: round05(upperSpacing),
+      //spacing: round05(upperSpacing),
+      spacing: upperSpacing,
       segments: upperSegments,
       //diff: round05(L - upperSegments * ideal)
-      diff: round05(upperDiff),
-      missing: round05(upperDiff)
+      //diff: round05(upperDiff),
+      diff: upperDiff,
+      //missing: round05(upperDiff)
+      missing: upperDiff
     }
   }
 }
