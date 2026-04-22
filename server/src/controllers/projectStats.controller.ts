69b99440bc5221ab5de6aa1ca5cbf7dc70c156d0
Volodymyr <volodymyr1988dev@gmail.com>
2026-04-22 22:35:42 +0200

    ProjectStats adminGuard

--- server/src/controllers/projectStats.controller.ts
+++ server/src/controllers/projectStats.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ProjectStatsService } from '../services/projectStats.service';
 import { ApiTags } from '@nestjs/swagger';
+import { UseGuards } from '@nestjs/common';
+import { AdminGuard } from '../types/auth/admin.guard';
 
 @ApiTags('Project stats')
 @Controller('projects')
 export class ProjectStatsController {
   constructor(private readonly statsService: ProjectStatsService) {}
 
+  @UseGuards(AdminGuard)
   @Get(':id/stats')
   getStats(@Param('id') id: string) {
     return this.statsService.getProjectStats(id);
