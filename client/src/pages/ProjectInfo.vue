1dac8d3f48fc447391bf3c22b0a4754d2042e386
Volodymyr <volodymyr1988dev@gmail.com>
2026-04-22 21:25:30 +0200

    ProjectInfo work extra admin only

--- client/src/pages/ProjectInfo.vue
+++ client/src/pages/ProjectInfo.vue
@@ -510,27 +510,27 @@ const totalAll = computed(() => totalWork.value + totalExtra.value)
       -->
       <div class="summary">
         <div 
-          @click="toggleSummary('work')" 
-          :class="{ active: showDetails === 'work' }"
+          @click="isAdmin && toggleSummary('work')" 
+          :class="{ active: showDetails === 'work', disabled: !isAdmin }"
         >
           Work <strong>{{ totalWork }}h</strong>
         </div>
 
         <div 
-          @click="toggleSummary('extra')" 
-          :class="{ active: showDetails === 'extra' }"
+          @click="isAdmin && toggleSummary('extra')" 
+          :class="{ active: showDetails === 'extra', disabled: !isAdmin }"
         >
           Extra <strong>{{ totalExtra }}h</strong>
         </div>
 
         <div 
-          @click="toggleSummary('total')" 
-          :class="{ active: showDetails === 'total' }"
+          @click="isAdmin && toggleSummary('total')" 
+          :class="{ active: showDetails === 'total', disabled: !isAdmin }"
         >
           Total <strong>{{ totalAll }}h</strong>
         </div>
       </div>
-      <div v-if="showDetails" class="project-details">
+      <div v-if="showDetails && isAdmin" class="project-details">
       <div v-if="loadingDetails">Loading...</div>
 
       <div v-else>
@@ -885,6 +885,14 @@ const totalAll = computed(() => totalWork.value + totalExtra.value)
   grid-column: span 3;
   font-size: 13px;
   color: #666;
+}
+.summary div.disabled {
+  cursor: default;
+  opacity: 0.6;
+}
+
+.summary div.disabled:hover {
+  background: transparent;
 }
 
 @media (max-width: 640px) {
