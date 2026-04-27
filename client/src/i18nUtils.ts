2921671d6fec70bddaad098a3a1d1f99424ee9b1
Volodymyr <volodymyr1988dev@gmail.com>
2026-04-27 09:18:58 +0200

    hope fix i18nUtils

--- client/src/i18nUtils.ts
+++ client/src/i18nUtils.ts
@@ -4,12 +4,33 @@ import dayjs from 'dayjs'
 
 const loadedLanguages = new Set<string>()
 
+const loaders = {
+  en: () => import('./locales/en'),
+  uk: () => import('./locales/uk'),
+  sv: () => import('./locales/sv'),
+  ro: () => import('./locales/ro'),
+  pl: () => import('./locales/pl'),
+}
+/*
 export async function setLanguage(locale: string) {
+  let lang = locale as keyof typeof loaders
   // якщо ще не завантажена — підтягуємо
+  if (!loaders[lang]) {
+    lang = 'en'
+  }
   if (!loadedLanguages.has(locale)) {
-    const messages = await import(`./locales/${locale}.ts`)
-    i18n.global.setLocaleMessage(locale, messages.default)
-    loadedLanguages.add(locale)
+    const loader = loaders[locale as keyof typeof loaders]
+
+    if (!loader) {
+      locale = 'en'
+    }
+    //const messages = await import(`./locales/${locale}.ts`)
+    const messages = await loaders[locale]()
+    //i18n.global.setLocaleMessage(locale, messages.default)
+    //loadedLanguages.add(locale)
+    i18n.global.locale.value = locale
+    dayjs.locale(locale)
+    localStorage.setItem('lang', locale)
   }
 
   // ставимо мову
@@ -19,7 +40,25 @@ export async function setLanguage(locale: string) {
   // зберігаємо
   localStorage.setItem('lang', locale)
 }
+*/
+export async function setLanguage(locale: string) {
+  let lang = locale as keyof typeof loaders
 
+  if (!loaders[lang]) {
+    lang = 'en'
+  }
+
+  if (!loadedLanguages.has(lang)) {
+    const messages = await loaders[lang]()
+    
+    i18n.global.setLocaleMessage(lang, messages.default)
+    loadedLanguages.add(lang)
+  }
+
+  i18n.global.locale.value = lang
+  dayjs.locale(lang)
+  localStorage.setItem('lang', lang)
+}
 export async function initLanguage() {
   const saved = localStorage.getItem('lang') || 'en'
   await setLanguage(saved)
