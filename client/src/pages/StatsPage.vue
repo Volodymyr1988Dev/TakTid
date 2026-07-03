2d2aab1da26438bf6adb4fe19949fd643d684bbc
Volodymyr <volodymyr1988dev@gmail.com>
2026-07-03 14:40:11 +0200

    StatsPage style 2

--- client/src/pages/StatsPage.vue
+++ client/src/pages/StatsPage.vue
@@ -699,13 +699,20 @@ button:active {
 
 .actions {
     display:flex;
-    gap:8px;
+    gap:20px;
 }
 
 .actions button{
     flex:1;
 }
-
+.date-group {
+    display: flex;
+    gap: 10px;
+  }
+.button-group {
+    display: flex;
+    gap: 10px;
+  }
 @media (max-width: 768px) {
   .date-controls{
     flex-direction: column;
@@ -714,10 +721,6 @@ button:active {
   }
   .calendar-grid {
     grid-template-columns: repeat(3, 1fr);
-  }
-  .date-group {
-    display: flex;
-    gap: 10px;
   }
   .button-group {
     display: flex;
