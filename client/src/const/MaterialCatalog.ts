69ae0eabac78a54fe2dc63faf9f9838534845136
Volodymyr <volodymyr1988dev@gmail.com>
2026-08-23 21:30:54 +0200

    new style MaterialList

--- client/src/const/MaterialCatalog.ts
+++ client/src/const/MaterialCatalog.ts
@@ -9,319 +9,313 @@ export type MaterialUnitKey =
 
 export interface MaterialDefinition {
   key: string
-  labelKey: string
+  label: string
   unitKey: MaterialUnitKey
 }
 
 export const MATERIAL_CATALOG: MaterialDefinition[] = [
   {
     key: 'gutter_6m',
-    labelKey: 'materials.gutter_6m',
+    label: 'Hängränna 6m',
     unitKey: 'units.piece',
   },
   {
     key: 'gutter_4m',
-    labelKey: 'materials.gutter_4m',
+    label: 'Hängränna 4m',
     unitKey: 'units.piece',
   },
   {
     key: 'gutter_3m',
-    labelKey: 'materials.gutter_3m',
+    label: 'Hängränna 3m',
     unitKey: 'units.piece',
   },
   {
     key: 'gutter_hook',
-    labelKey: 'materials.gutter_hook',
+    label: 'Rännkrok',
     unitKey: 'units.piece',
   },
   {
     key: 'gutter_end',
-    labelKey: 'materials.gutter_end',
+    label: 'Ränngavel',
     unitKey: 'units.piece',
   },
   {
     key: 'gutter_joint',
-    labelKey: 'materials.gutter_joint',
+    label: 'Rännskarv',
     unitKey: 'units.piece',
   },
   {
     key: 'outer_gutter_angle',
-    labelKey: 'materials.outer_gutter_angle',
+    label: 'Rännvinkel ytter',
     unitKey: 'units.piece',
   },
   {
     key: 'inner_gutter_angle',
-    labelKey: 'materials.inner_gutter_angle',
+    label: 'Rännvinkel inner',
     unitKey: 'units.piece',
   },
   {
     key: 'omvik',
-    labelKey: 'materials.omvik',
+    label: 'Omvik (omvikningskupa)',
     unitKey: 'units.piece',
   },
-
   {
     key: 'downpipe_2_5m',
-    labelKey: 'materials.downpipe_2_5m',
+    label: 'Stuprör 2.5m',
     unitKey: 'units.piece',
   },
   {
     key: 'downpipe_3m',
-    labelKey: 'materials.downpipe_3m',
+    label: 'Stuprör 3m',
     unitKey: 'units.piece',
   },
   {
     key: 'downpipe_4m',
-    labelKey: 'materials.downpipe_4m',
+    label: 'Stuprör 4m',
     unitKey: 'units.piece',
   },
   {
     key: 'downpipe_6m',
-    labelKey: 'materials.downpipe_6m',
+    label: 'Stuprör 6m',
     unitKey: 'units.piece',
   },
   {
     key: 'downpipe_clamp',
-    labelKey: 'materials.downpipe_clamp',
+    label: 'Stuprörssvep',
     unitKey: 'units.piece',
   },
   {
     key: 'leaf_strainer',
-    labelKey: 'materials.leaf_strainer',
+    label: 'Lövsil',
     unitKey: 'units.piece',
   },
   {
     key: 'pipe_angle',
-    labelKey: 'materials.pipe_angle',
+    label: 'Rörvinkel',
     unitKey: 'units.piece',
   },
   {
     key: 'outlet',
-    labelKey: 'materials.outlet',
+    label: 'Utkastare',
     unitKey: 'units.piece',
   },
 
   {
     key: 'windboard_22x170_3_6m',
-    labelKey: 'materials.windboard_22x170_3_6m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x170 3.6m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_22x170_4_8m',
-    labelKey: 'materials.windboard_22x170_4_8m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x170 4.8m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_22x170_4_2m',
-    labelKey: 'materials.windboard_22x170_4_2m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x170 4.2m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_22x170_5_4m',
-    labelKey: 'materials.windboard_22x170_5_4m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x170 5.4m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_wood',
-    labelKey: 'materials.windboard_wood',
+    label: 'Ytterpanelbräda (vindskivor trä)',
     unitKey: 'units.meter',
   },
   {
     key: 'windboard_22x195_4_8m',
-    labelKey: 'materials.windboard_22x195_4_8m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x195 4.8m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_22x195_5_4m',
-    labelKey: 'materials.windboard_22x195_5_4m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x195 5.4m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_22x195_3_6m',
-    labelKey: 'materials.windboard_22x195_3_6m',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x195 3.6m',
     unitKey: 'units.piece',
   },
   {
     key: 'windboard_mixed',
-    labelKey: 'materials.windboard_mixed',
+    label: 'Ytterpanelbräda (vindskivor trä) 22x170 22x120 22x195 22x145',
+    unitKey: 'units.piece',
+  },
+  {
+    key: 'windboard_metal',
+    label: 'Vindskiveplåt',
     unitKey: 'units.piece',
   },
 
   {
-    key: 'windboard_metal',
-    labelKey: 'materials.windboard_metal',
-    unitKey: 'units.piece',
-  },
-  {
     key: 'roof_ladder_step',
-    labelKey: 'materials.roof_ladder_step',
+    label: 'Bärläktsteg',
     unitKey: 'units.piece',
   },
   {
     key: 'snow_guard',
-    labelKey: 'materials.snow_guard',
+    label: 'Glidskydd',
     unitKey: 'units.piece',
   },
 
   {
     key: 'roof_sheathing_23x540_3_6m',
-    labelKey: 'materials.roof_sheathing_23x540_3_6m',
+    label: 'Råspontlucka 23x540 3.6m',
     unitKey: 'units.piece',
   },
   {
     key: 'roof_sheathing_23x540_4_2m',
-    labelKey: 'materials.roof_sheathing_23x540_4_2m',
+    label: 'Råspontlucka 23x540 4.2m',
     unitKey: 'units.piece',
   },
   {
     key: 'roof_sheathing_20x540_3_6m',
-    labelKey: 'materials.roof_sheathing_20x540_3_6m',
+    label: 'Råspontlucka 20x540 3.6m',
     unitKey: 'units.piece',
   },
   {
     key: 'roof_sheathing_20x540_4_2m',
-    labelKey: 'materials.roof_sheathing_20x540_4_2m',
+    label: 'Råspontlucka 20x540 4.2m',
     unitKey: 'units.piece',
   },
   {
     key: 'roof_sheathing_20x540_4_8m',
-    labelKey: 'materials.roof_sheathing_20x540_4_8m',
+    label: 'Råspontlucka 20x540 4.8m',
     unitKey: 'units.piece',
   },
-
   {
     key: 'roof_sheathing_17mm',
-    labelKey: 'materials.roof_sheathing_17mm',
+    label: 'Råspont 17mm single',
     unitKey: 'units.piece',
   },
   {
     key: 'roof_sheathing_19mm',
-    labelKey: 'materials.roof_sheathing_19mm',
+    label: 'Råspont 19mm single',
     unitKey: 'units.piece',
   },
 
   {
     key: 'silicone',
-    labelKey: 'materials.silicone',
+    label: 'Tek7 / Silicone',
     unitKey: 'units.tube',
   },
   {
     key: 'pistol_nails',
-    labelKey: 'materials.pistol_nails',
+    label: 'Nails for pistol',
     unitKey: 'units.pack',
   },
   {
     key: 'screws_55mm',
-    labelKey: 'materials.screws_55mm',
+    label: 'Screws 55mm',
     unitKey: 'units.piece',
   },
   {
     key: 'metal_screws_33mm',
-    labelKey: 'materials.metal_screws_33mm',
+    label: '33mm plåtskruv',
     unitKey: 'units.piece',
   },
   {
     key: 'screws_42mm',
-    labelKey: 'materials.screws_42mm',
+    label: '42mm skruv',
     unitKey: 'units.piece',
   },
   {
     key: 'wood_screws_30mm',
-    labelKey: 'materials.wood_screws_30mm',
+    label: '30mm träskruv',
     unitKey: 'units.piece',
   },
   {
     key: 'screw_120mm',
-    labelKey: 'materials.screw_120mm',
+    label: 'Skruv 120mm',
     unitKey: 'units.piece',
   },
   {
     key: 'screw_75_80mm',
-    labelKey: 'materials.screw_75_80mm',
+    label: 'Skruv 75 eller 80mm',
     unitKey: 'units.piece',
   },
   {
     key: 'wind_screw',
-    labelKey: 'materials.wind_screw',
+    label: 'Vindskruva',
     unitKey: 'units.piece',
   },
   {
     key: 'farmers_screw',
-    labelKey: 'materials.farmers_screw',
+    label: 'Farmarskruv',
     unitKey: 'units.piece',
   },
   {
     key: 'clips',
-    labelKey: 'materials.clips',
+    label: 'Clips',
     unitKey: 'units.piece',
   },
-
   {
     key: 'ridge_band',
-    labelKey: 'materials.ridge_band',
+    label: 'Nockband',
     unitKey: 'units.meter',
   },
   {
     key: 'bird_band',
-    labelKey: 'materials.bird_band',
+    label: 'Fågelband',
     unitKey: 'units.piece',
   },
   {
     key: 'eave_flashing_2m',
-    labelKey: 'materials.eave_flashing_2m',
+    label: 'Fotplåt 2m',
     unitKey: 'units.piece',
   },
-
   {
     key: 'batten_25x48',
-    labelKey: 'materials.batten_25x48',
+    label: 'Läkt standard 25x48',
     unitKey: 'units.piece',
   },
   {
     key: 'batten_12x50',
-    labelKey: 'materials.batten_12x50',
+    label: 'Läkt tunn 12x50',
     unitKey: 'units.piece',
   },
   {
     key: 'triangle_batten',
-    labelKey: 'materials.triangle_batten',
+    label: 'Trekantsläkt',
     unitKey: 'units.meter',
   },
   {
     key: 'rafter',
-    labelKey: 'materials.rafter',
+    label: 'Regel',
     unitKey: 'units.meter',
   },
-
   {
     key: 'rafter_45x45_4_8m',
-    labelKey: 'materials.rafter_45x45_4_8m',
+    label: 'Gran hyvlad regel O/S V 45x45 4,8 m',
     unitKey: 'units.piece',
   },
   {
     key: 'rafter_45x45_3_6m',
-    labelKey: 'materials.rafter_45x45_3_6m',
+    label: 'Gran hyvlad regel O/S V 45x45 3,6 m',
     unitKey: 'units.piece',
   },
   {
     key: 'short_rafter_45x45_2_5m',
-    labelKey: 'materials.short_rafter_45x45_2_5m',
+    label: 'Gran vilmaregel kortregel 45x45 2.5 m',
     unitKey: 'units.piece',
   },
-
   {
     key: 'trash_bags',
-    labelKey: 'materials.trash_bags',
+    label: 'Sopsäckar',
     unitKey: 'units.piece',
   },
   {
     key: 'paint',
-    labelKey: 'materials.paint',
+    label: 'Färg',
     unitKey: 'units.bucket',
   },
   {
     key: 'tape',
-    labelKey: 'materials.tape',
+    label: 'Tejp',
     unitKey: 'units.roll',
   },
 ]
