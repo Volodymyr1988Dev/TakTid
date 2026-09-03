84a6cf4eb5251b21a8027f6bbf5a135f549500f2
Volodymyr <volodymyr1988dev@gmail.com>
2026-09-03 17:18:36 +0200

    delete authApiClient

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -1,20 +1,20 @@
 import api from './axios';
-import axios from 'axios';
+//import axios from 'axios';
 
 export const register = (data: {
   email: string;
   password: string;
   name: string;
 }) => api.post('/auth/register', data);
-
+/*
 const authApiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
+*/
 export const login = (data: { email: string; password: string }) =>
   api.post('/auth/login', data);
 export const me = () => api.get('/auth/me');
 export const logout = () => api.post('/auth/logout');
 //export const refresh = () => api.post('/auth/refresh');
-export const refresh = () => authApiClient.post('/auth/refresh');
+export const refresh = () => /*authApiClient*/ api.post('/auth/refresh');
